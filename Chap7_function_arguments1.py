ddcb41ad7bd2df52516cf15d3deef9c1cf983974
stankevo1 <elena.st463@gmail.com>
2019-09-05 14:49:33 +0200

    Chapter 7 - functions Part 2 - minor changes

--- Chap7_function_arguments1.py
+++ Chap7_function_arguments1.py
@@ -3,6 +3,7 @@ def main():
     print(f'in main x is {x}')
     print(f'id x is {id(x)}')
     kitten(x)
+    print(f'after calling a function id x is still {id(x)}')
 
 def kitten(a):
     print(f'id a is {id(a)}')
