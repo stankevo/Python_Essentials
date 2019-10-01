00352aecdec8a3bf42818d03b4281675688bdc52
stankevo1 <elena.st463@gmail.com>
2019-10-01 18:14:08 +0200

    Chapter 8 - mixed structures Part3

--- Chap8_mixed_structures.py
+++ Chap8_mixed_structures.py
@@ -1,3 +1,7 @@
+#globals
+
+dlevel = 0
+
 def main():
     r = range(11)
     l = [1, 'two', 3, {'4':'four'}, 5]
@@ -5,50 +9,44 @@ def main():
     s = set("It's a bird! It's a plane! It's a Superman!")
     d = dict(one = r, two = l, three = s)
     mixed = [l, r, s, d, t]
-    print(l)
-    print(r)
-    print(s)
-    print(d)
-    print(t)
-
-    print()
-    print_list(l)
-    print_list(r)
-    print_set(s)
-    print_tuple(t)
-    print_dict(d)
-
-    print('\n----------------------------')
     disp(mixed)
 
 def disp(o):
+    global dlevel
+
+    dlevel += 1
     if   isinstance(o, list): print_list(o)
     elif isinstance(o, range): print_list(o)
     elif isinstance(o, set):  print_set(o)
     elif isinstance(o, dict): print_dict(o)
     elif isinstance(o, tuple): print_tuple(o)
-    elif o is 'None': print('Nada', end = ' ')
+    elif o is None: print('Nada', end = ' ')
     else: print(repr(o), end = ' ')
+    dlevel -= 1
+
+    if dlevel <= 1: print()
 
 def print_list(o):
-    print('[', end = '')
-    for i in o: disp(i) #print(i, end = ' ')
-    print(']')
+    print('[', end = ' ')
+    for i in o: disp(i)
+    print(']', end = ' ')
 
 def print_set(o):
-    print('{', end = '')
-    for i in sorted(o): disp(i) #print(i, end = ' ')
-    print('}')
+    print('{', end = ' ')
+    for i in sorted(o): disp(i)
+    print('}', end = ' ')
 
 def print_tuple(o):
-    print('(', end = '')
-    for i in o: disp(i) #print(i, end = ' ')
-    print(')')
+    print('(', end = ' ')
+    for i in o: disp(i)
+    print(')', end = ' ')
 
 def print_dict(o):
-    print('{', end = '')
-    for k,v in o.items(): print(f'{k}: {v}', end = ' ')
-    print('}')
+    print('{', end = ' ')
+    for k,v in o.items():
+        print(k, end = ': ')
+        disp(v)
+    print('}', end = ' ')
 
 
 if __name__ == '__main__': main()
