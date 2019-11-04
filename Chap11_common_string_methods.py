2927cbe973fa0d41dcfbc163015646438dfe3c76
Elena <elena.st463@gmail.com>
2019-11-04 21:55:29 +0100

    Chapter 11 - common string methods

---
+++ Chap11_common_string_methods.py
@@ -0,0 +1,24 @@
+s1 = 'Hello, world!'
+print(s1)
+s2 = s1.swapcase()
+print(s2)
+s3 = s1.title()
+print(s3)
+s4= s1.upper()
+print(s4)
+s5 = s1.lower()
+print(s5)
+s6 = s1.capitalize()
+print(s6)
+s7 = s1.casefold()
+print(s7)
+s8 = 'another string'
+s9 = s1 + ' ' + s8
+
+print(id(s1))
+print(id(s2))
+
+print('hello, world!'.upper())
+
+s10 = 'this string' ' that string'
+print(s10)
