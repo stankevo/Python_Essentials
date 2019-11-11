da9728ef3cb9668e1bf79178f9fdb3d0b2eb8c01
stankevo1 <elena.st463@gmail.com>
2019-11-11 16:32:51 +0100

    Chapter 14 - convert number to words 2

--- Chap14_convert_numbers_to_words.py
+++ Chap14_convert_numbers_to_words.py
@@ -1,18 +1,51 @@
-class numword:
-    def __init__(self, n):
+# fix: 20: twenty-zero
+
+class numToWord:
+    def __init__(self, n = None):
         self._number = n
+        self._word = 'None'
+
+        self._dict20 = {
+            0: 'zero',      1: 'one',        2: 'two',       3: 'three',     4: 'four', 5: 'five', 
+            6: 'six',       7: 'seven',      8: 'eight',     9: 'nine',     10: 'ten',
+            11: 'eleven',  12: 'twelve',    13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
+            16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+
+        self._dictTens = {
+            20: 'twenty', 30: 'thirty', 40: 'fourghty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
+            90: 'ninety'}
+
+        self._word = self.getword(n)
+
 
     def __repr__(self):
-        return f'{self._number}'
+        return f'{self._word}'
 
-    def number(self):
+    def number(self, n = None):
+        if n: self._number = n
         return self._number
 
+    def word(self):
+        return self._word
+
+    def getword(self, n):
+        if n >= 0 and n <= 19:
+            w = self._dict20[n]
+        elif n < 100:
+            a = (n // 10) * 10
+            b = n % 10
+            w = self._dictTens[a] + '-' + self._dict20[b] 
+        return w
+
 def main():
-    a = numword(5)
+    a = numToWord(79)
     print(a)
     b = a.number()
-    print(b)
+    c = a.word()
+    print(b, a)
+
+    for i in range(100):
+        print(f'{i}: {numToWord(i)}')
     
 
 if __name__ == '__main__': main()
