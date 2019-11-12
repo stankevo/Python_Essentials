07a0bd3979197ba61430038ec8b883279594c7d8
stankevo1 <elena.st463@gmail.com>
2019-11-12 13:43:47 +0100

    Chapter 14 - convert numbers to words part3

--- Chap14_convert_numbers_to_words.py
+++ Chap14_convert_numbers_to_words.py
@@ -6,14 +6,14 @@ class numToWord:
         self._word = 'None'
 
         self._dict20 = {
-            0: 'zero',      1: 'one',        2: 'two',       3: 'three',     4: 'four', 5: 'five', 
-            6: 'six',       7: 'seven',      8: 'eight',     9: 'nine',     10: 'ten',
-            11: 'eleven',  12: 'twelve',    13: 'thirteen', 14: 'fourteen', 15: 'fifteen',
-            16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
+            0: 'zero',      1: 'one',      2: 'two',        3: 'three',     4: 'four', 
+            5: 'five',      6: 'six',      7: 'seven',      8: 'eight',     9: 'nine',
+            10: 'ten',     11: 'eleven',  12: 'twelve',    13: 'thirteen', 14: 'fourteen',
+            15: 'fifteen', 16: 'sixteen', 17: 'seventeen', 18: 'eighteen', 19: 'nineteen'}
 
         self._dictTens = {
-            20: 'twenty', 30: 'thirty', 40: 'fourghty', 50: 'fifty', 60: 'sixty', 70: 'seventy', 80: 'eighty',
-            90: 'ninety'}
+            20: 'twenty', 30: 'thirty',  40: 'forty',  50: 'fifty',
+            60: 'sixty',  70: 'seventy', 80: 'eighty', 90: 'ninety'}
 
         self._word = self.getword(n)
 
@@ -29,23 +29,30 @@ class numToWord:
         return self._word
 
     def getword(self, n):
+        if n < 0 or n >= 100:
+            raise TypeError(f'Expected value between 0 and 99, got {n}.')
         if n >= 0 and n <= 19:
             w = self._dict20[n]
-        elif n < 100:
+        elif n % 10 == 0:
+            w = self._dictTens[n]
+        else:
             a = (n // 10) * 10
             b = n % 10
-            w = self._dictTens[a] + '-' + self._dict20[b] 
+            w = self._dictTens[a] + '-' + self._dict20[b]
         return w
 
 def main():
-    a = numToWord(79)
-    print(a)
-    b = a.number()
-    c = a.word()
-    print(b, a)
-
+    x = 100
+    try:
+        a = numToWord(x)
+        print(a)
+        b = a.number()
+        c = a.word()
+        print(b, a)
+    except TypeError as e:
+        print(f'Error: {e}')
+    # test
     for i in range(100):
         print(f'{i}: {numToWord(i)}')
     
-
 if __name__ == '__main__': main()
